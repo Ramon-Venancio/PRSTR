e428a12f6a6e9f22f40f07c748e5ec7e8a6740d7
Ramon-Venancio <ramonvenanciosousa@gmail.com>
2022-05-07 17:07:37 -0300

    só uma alteração

--- aula2/teste-while.py
+++ aula2/teste-while.py
@@ -1,6 +1,6 @@
-condicao = True
-while(condicao):
-    print("BlOCO while() e condicao**True")
-    condicao = False
+conta = 0
+while conta <= 10:
+    conta += 1
+    print("Rodando while")
 else:
-    print("BLOCO ELSE e condicao**False")
+    print("Valor da variavel conta: ", conta)
--- aula2/teste-while2.py
+++
@@ -1,6 +0,0 @@
-conta = 0
-while conta <= 10:
-    conta += 1
-    print("Rodando while")
-else:
-    print("Valor da variavel conta: ", conta)
